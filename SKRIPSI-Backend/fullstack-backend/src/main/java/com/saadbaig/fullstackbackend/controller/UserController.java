ce753784e1f77578d036f0e4f0a6fcc142a19422
Sansan Fajrian <sansan.fajrian@gmail.com>
2024-03-06 21:18:36 +0700

    DEACTIVATE MINIO

--- SKRIPSI-Backend/fullstack-backend/src/main/java/com/saadbaig/fullstackbackend/controller/UserController.java
+++ SKRIPSI-Backend/fullstack-backend/src/main/java/com/saadbaig/fullstackbackend/controller/UserController.java
@@ -1,12 +1,22 @@
 package com.saadbaig.fullstackbackend.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.saadbaig.fullstackbackend.model.User;
 import com.saadbaig.fullstackbackend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import com.saadbaig.fullstackbackend.controller.UserNotFoundException;
 
-import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -15,9 +25,32 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    // @Autowired
+    // private MinioClient minioClient;
+
     @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
-        return userRepository.save(newUser);
+        try {
+            // Convert String data to InputStream
+            InputStream dataStream = new ByteArrayInputStream(newUser.toString().getBytes());
+
+            // minioClient.putObject(
+            //         PutObjectArgs.builder()
+            //                 .bucket("bucket-sansan")
+            //                 .object(newUser.getUsername() + ".json") // Use a unique identifier for each user
+            //                 .stream(dataStream, dataStream.available(), -1) // Use available() to get the length of the stream
+            //                 .build()
+            // );
+
+            // Return the saved user after successful MinIO upload
+            return userRepository.save(newUser);
+        } catch (Exception e) {
+            // Handle MinIO upload exception
+            e.printStackTrace();
+            
+            // Handle the case where MinIO upload fails - you might want to throw an exception or return a specific response
+            return null; // Adjust this accordingly based on your error handling strategy
+        }
     }
 
     @GetMapping("/users")
